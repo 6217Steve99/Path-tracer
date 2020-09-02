37249fed3aa8314602219e7110287f20b573f393
Zachary <zcy621799@163.com>
2020-09-02 23:55:05 +0800

    SceneLoader

---
+++ Path tracer/SceneLoader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Geometry.h"
+
+extern unsigned verticesNo;
+extern Vertex* vertices;
+extern unsigned int trianglesNo;
+extern Triangle* triangles;
+
+void panic(const char* fmt, ...);
+void load_object(const char* filename);
+float processgeo();
